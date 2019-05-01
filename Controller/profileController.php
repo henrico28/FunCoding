7eedcfc4a748199f711f91c348028f55d712dabd
Harry Senjaya <harxsenjaya56@gmail.com>
2019-05-01 15:23:23 +0700

    profil

--- Controller/profileController.php
+++ Controller/profileController.php
@@ -9,17 +9,22 @@
         
         public function start(){
             session_start();
-            $user = $_SESSION['username'];
-            echo $_SESSION['username'];
+            $user = $_SESSION['userlogin'];
             session_write_close();
             
-            $query="SELECT NamaPengguna,Username,Negara,Email FROM pengguna WHERE Username = '$user'";
-            $res = $this->db->executeSelectQuery($query);
+            $data="SELECT NamaPengguna,Username,Negara,Email FROM pengguna WHERE NamaPengguna = '$user'";
+            $res = $this->db->executeSelectQuery($data);
+
+            $history = "SELECT NamaBahasa,NamaLevel,Skor FROM Ujian JOIN MasterSoal ON Ujian.IdMasterSoal = MasterSoal.IdMasterSoal
+            JOIN Bahasa ON MasterSoal.IdBahasa = Bahasa.IdBahasa JOIN Level ON MasterSoal.IdLevel = Level.IdLevel JOIN Pengguna ON Ujian.Username = Pengguna.Username WHERE NamaPengguna = '$user'";
+            $res2 = $this->db->executeSelectQuery($history);
+
             return View::createView('profile.php',[
                 "nama" => $res[0][0],
                 "user" => $res[0][1],
                 "negara" => $res[0][2],
-                "email" => $res[0][3]
+                "email" => $res[0][3],
+                "history" => $res2
             ]);
         }
     }
