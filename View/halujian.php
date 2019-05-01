1a8537ac565f2285c83240cc6218cbc29e8a1b5d
YovanJong <50098631+YovanJong@users.noreply.github.com>
2019-05-01 16:50:52 +0700

    halaman soal user

---
+++ View/halujian.php
@@ -0,0 +1,85 @@
+<html>
+    <head>
+        <link rel="stylesheet" href="styles/styleshalujian.css">
+    </head>
+    <body>
+        <div class="flex-haluji">
+            <div>           
+                <form>
+                    <h1>Soal Ujian</h1>
+                    <p>
+                        1. jdaklsfjlkadjflajjfdalkfdjaskfljd
+                        asdjfkljadklfjadskl
+                        kadjsfkldjafkljadlkf<br>
+                    </p>
+
+                    <select>
+                        <option value = "">Java</option>
+                        <option value = "">HTML</option>
+                        <option value = "">C++</option>
+                        <option value = "">CSS</option>
+                    </select>
+                    <br><br>
+
+                    <p>
+                        2. jdaklsfjlkadjflajjfdalkfdjaskfljd
+                        asdjfkljadklfjadskl
+                        kadjsfkldjafkljadlkf<br>
+                    </p>
+
+                    <select>
+                        <option value = "">Java</option>
+                        <option value = "">HTML</option>
+                        <option value = "">C++</option>
+                        <option value = "">CSS</option>
+                    </select>
+                    <br><br>
+
+                    <p>
+                        3. jdaklsfjlkadjflajjfdalkfdjaskfljd
+                        asdjfkljadklfjadskl
+                        kadjsfkldjafkljadlkf<br>
+                    </p>
+
+                    <select>
+                        <option value = "">Java</option>
+                        <option value = "">HTML</option>
+                        <option value = "">C++</option>
+                        <option value = "">CSS</option>
+                    </select>
+                    <br><br>
+
+                    <p>
+                        4. jdaklsfjlkadjflajjfdalkfdjaskfljd
+                        asdjfkljadklfjadskl
+                        kadjsfkldjafkljadlkf<br>
+                    </p>
+
+                    <select>
+                        <option value = "">Java</option>
+                        <option value = "">HTML</option>
+                        <option value = "">C++</option>
+                        <option value = "">CSS</option>
+                    </select>
+                    <br><br>
+
+                    <p>
+                        5. jdaklsfjlkadjflajjfdalkfdjaskfljd
+                        asdjfkljadklfjadskl
+                        kadjsfkldjafkljadlkf<br>
+                    </p>
+
+                    <select>
+                        <option value = "">Java</option>
+                        <option value = "">HTML</option>
+                        <option value = "">C++</option>
+                        <option value = "">CSS</option>
+                    </select>
+                    <br><br>
+
+                    <input type="submit" value="Submit">
+                </form>
+            </div>
+        </div>
+    </body>
+</html>
