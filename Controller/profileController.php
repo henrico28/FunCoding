80276a84f5ff10478880071b68c05fffc64f97f3
Henrico <henricoleodra@gmail.com>
2019-05-01 15:54:44 +0700

    Fix Username

--- Controller/profileController.php
+++ Controller/profileController.php
@@ -9,13 +9,13 @@
         
         public function start(){
             session_start();
-            $user = $_SESSION['userlogin'];
+            $user = $_SESSION['username'];
             session_write_close();
-            $data="SELECT NamaPengguna,Username,Negara,Email FROM pengguna WHERE NamaPengguna = '$user'";
+            $data="SELECT NamaPengguna,Username,Negara,Email FROM pengguna WHERE Username = '$user'";
             $res = $this->db->executeSelectQuery($data);
 
             $history = "SELECT NamaBahasa,NamaLevel,Skor FROM Ujian JOIN MasterSoal ON Ujian.IdMasterSoal = MasterSoal.IdMasterSoal
-            JOIN Bahasa ON MasterSoal.IdBahasa = Bahasa.IdBahasa JOIN Level ON MasterSoal.IdLevel = Level.IdLevel JOIN Pengguna ON Ujian.Username = Pengguna.Username WHERE NamaPengguna = '$user'";
+            JOIN Bahasa ON MasterSoal.IdBahasa = Bahasa.IdBahasa JOIN Level ON MasterSoal.IdLevel = Level.IdLevel JOIN Pengguna ON Ujian.Username = Pengguna.Username WHERE Username = '$user'";
             $res2 = $this->db->executeSelectQuery($history);
 
             return View::createView('profile.php',[
