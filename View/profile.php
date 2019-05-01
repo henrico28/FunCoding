0fce1eeed62ac5f1b51c4b4ab19dc6a38657700f
Henrico <henricoleodra@gmail.com>
2019-05-01 12:28:17 +0700

    Upload File

---
+++ View/profile.php
@@ -0,0 +1,26 @@
+<html>
+    <head>
+        <link rel="stylesheet" href="css/styleprofile.css">
+    </head>
+    <body>
+        <div class="flex-profile">
+            <div>
+                <h1>Profile</h1>
+                <hr>
+                Nama lengkap: <br>
+                Username: <br>
+                Password: <br>
+                Asal negara: <br>
+                email: <br>
+            </div>
+        </div>
+        <div class="flex-history">
+            <div>
+                <h1>History</h1>
+                <hr>
+                Java - Beginner - 80 <br>
+                Java - Beginner - 100 <br>
+            </div>
+        </div>
+    </body>
+</html>
---
+++ View/result.php
@@ -0,0 +1,22 @@
+<html>
+    <head>
+        <link rel="stylesheet" href="css/styleresult.css">
+    </head>
+    <body>
+        <div class="flex-body">
+            <div id="box">
+                <h1>RESULT</h1>
+                <hr>
+                Nama: <br>
+                Username: <br>
+                Country: <br>
+                Language: <br>
+                level: <br>
+                Score: <br>
+                <hr>
+                <a href=""><input id="buttons" type="button" value="Ok"></a>
+                <br> 
+            </div>
+        </div>
+    </body>
+</html>
