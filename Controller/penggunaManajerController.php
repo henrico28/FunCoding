5142e116c86e5bad46ce83a71b02371beddfe995
Henrico <henricoleodra@gmail.com>
2019-05-03 19:17:05 +0700

    pagination beres

--- Controller/penggunaManajerController.php
+++ Controller/penggunaManajerController.php
@@ -12,15 +12,25 @@
 			$nama = $_SESSION['userlogin'];
             session_write_close();
 
-            $data="SELECT NamaPengguna,Negara,Email FROM pengguna WHERE IdPosisi = 3";
-            $res = $this->db->executeSelectQuery($data);
+            $start = 0;
+            $query="SELECT NamaPengguna,Negara,Email FROM pengguna WHERE IdPosisi = 3";
+            $res = $this->db->executeSelectQuery($query);
+            $show = 5;
+            $page = count($res) / $show;
+            if(isset($_GET['start'])){
+                $start=$this->db->escapeString($_GET['start']);
+            }
+            $query.=" LIMIT $start,$show";
+            $result = $this->db->executeSelectQuery($query);
 
             $top10 = "SELECT Pengguna.Username,Pengguna.NamaPengguna,COUNT(Pengguna.Username) AS 'jumlahSertifikat' FROM Sertifikat JOIN Pengguna ON Sertifikat.Username = Pengguna.Username GROUP BY Username ORDER BY 'jumlahSertifikat' DESC";
             $res2 = $this->db->executeSelectQuery($top10);
             return View::createView('hal_pengguna-manajer.php',[
                 "nama" => $nama,
-                "data" => $res,
-                "top10" => $res2
+                "data" => $result,
+                "top10" => $res2,
+                "page"=>$page,
+                "query"=>$query
             ]);
         }
 	}
