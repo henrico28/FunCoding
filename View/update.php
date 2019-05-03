75ac06e7c97ae23fa4bb23a5e70055d3dab98945
Henrico <henricoleodra@gmail.com>
2019-05-03 12:49:05 +0700

    Fix Update

--- View/update.php
+++ View/update.php
@@ -1,17 +1,14 @@
 <div class="flex-body">
             <div>
                 <h1>UPDATE PASSWORD</h1>
-                <form method="POST" action="signin">
+                <form method="POST" action="update">
                     <a href="index.php"><img src="View/images/logo.png"></a> <br>
                     <label>New Password</label> <br>
                     <input class = "txt" type="password" name="npass" placeholder="Enter New Password" pattern=".{5,}" required title="5 characters minimum"> <br>
                     <label>Confirm Password<label> <br>
                     <input class = "txt" type="password" name="cpass" placeholder="Enter Confirm Password" pattern=".{5,}" required title="5 characters minimum"> <br>
                     <br><br>
-                    <input id="buttons" type="submit" value="Login"> <br>
-                    <hr>
-                    <a href="signup"><input id="buttons" type="button" value="Sign Up"></a>
-                    <br><br>       
+                    <input id="buttons" type="button" value="Change Password"> <br>      
                 </form>
             </div>
         </div>
