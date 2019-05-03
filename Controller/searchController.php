4c4d55a76bc0910040ae8c946ce5bac0efa20b1d
Harry Senjaya <harxsenjaya56@gmail.com>
2019-05-03 19:45:28 +0700

    search

--- Controller/searchController.php
+++ Controller/searchController.php
@@ -11,47 +11,21 @@
 			$bahasa = $_POST['language'];
 			$level = $_POST['level'];
 			session_start();
-			$user = $_SESSION['username'];
+			$_SESSION['language'] = $bahasa;
+			$_SESSION['lvl'] = $level;
+			$nama = $_SESSION['userlogin'];
 			session_write_close();
-			$qValidate = "SELECT NamaBahasa, NamaLevel, Skor FROM Ujian JOIN MasterSoal ON Ujian.IdMasterSoal = MasterSoal.IdMasterSoal JOIN Bahasa ON MasterSoal.IdBahasa = Bahasa.IdBahasa JOIN lvl ON MasterSoal.IdLevel = lvl.IdLevel WHERE Username = '$user',NamaBahasa != '$bahasa', NamaLevel != '$level'";
-			$tmp = $this->db->executeSelectQuery($qValidate);
-			if(count($tmp)!=0){
-				session_start();
-				$_SESSION['language'] = $bahasa;
-				$_SESSION['lvl'] = $level;
-				$nama = $_SESSION['userlogin'];
-				session_write_close();
-				$query = "SELECT IdSoal, soal, A, B, C, D, JawabanSoal, Soal.IdMasterSoal FROM( SELECT IdMasterSoal FROM bahasa JOIN mastersoal ON mastersoal.IdBahasa = bahasa.IdBahasa JOIN lvl ON mastersoal.IdLevel = lvl.IdLevel WHERE bahasa.NamaBahasa = '$bahasa' AND lvl.NamaLevel = '$level' )AS himpBaru JOIN soal ON himpBaru.IdMasterSoal = soal.IdMasterSoal";
-				$resSoal = $this->db->executeSelectQuery($query);
-				session_start();
-				$_SESSION['ressoal']=$resSoal;
-				session_write_close();
-				return View::createView('halujian.php',[
-					"nama"=> $nama,
-					"bahasa"=> $bahasa,
-					"level"=> $level,
-					"soal"=> $resSoal
-				]);
-			}
-			else{
-				?>
-				<script>
-					alert("You have attempt this examination, Sorry no retakes!");
-				</script>
-				<?php
-				session_start();
-				$nama = $_SESSION['userlogin'];
-				$b = $_SESSION['bahasa'];
-				$l = $_SESSION['level'];
-				session_write_close();
-				return View::createView('homeuser.php',[
-					"nama"=> $nama,
-					"bahasa"=> $b,
-					"level"=> $l
-				]);
-
-			}
-			
+			$query = "SELECT IdSoal, soal, A, B, C, D, JawabanSoal, Soal.IdMasterSoal FROM( SELECT IdMasterSoal FROM bahasa JOIN mastersoal ON mastersoal.IdBahasa = bahasa.IdBahasa JOIN lvl ON mastersoal.IdLevel = lvl.IdLevel WHERE bahasa.NamaBahasa = '$bahasa' AND lvl.NamaLevel = '$level' )AS himpBaru JOIN soal ON himpBaru.IdMasterSoal = soal.IdMasterSoal";
+			$resSoal = $this->db->executeSelectQuery($query);
+			session_start();
+			$_SESSION['ressoal']=$resSoal;
+			session_write_close();
+            return View::createView('halujian.php',[
+				"nama"=> $nama,
+				"bahasa"=> $bahasa,
+				"level"=> $level,
+				"soal"=> $resSoal
+            ]);
         }
 	}
 ?>
