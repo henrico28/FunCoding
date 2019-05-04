e16eeb673f61e598b7c45bee998b0d25049d284a
Henrico <henricoleodra@gmail.com>
2019-05-04 09:29:26 +0700

    Avg Durasi

--- Controller/statistikController.php
+++ Controller/statistikController.php
@@ -12,7 +12,7 @@
 			$nama = $_SESSION['userlogin'];
             session_write_close();
 
-            $statistik = "SELECT Bahasa.NamaBahasa,lvl.NamaLevel,AVG(Ujian.Skor) FROM Ujian JOIN MasterSoal ON Ujian.IdMasterSoal = MasterSoal.IdMasterSoal JOIN Bahasa ON MasterSoal.IdBahasa = Bahasa.IdBahasa JOIN lvl ON  MasterSoal.IdLevel = lvl.IdLevel GROUP BY MasterSoal.IdMasterSoal";
+            $statistik = "SELECT Bahasa.NamaBahasa,lvl.NamaLevel,AVG(Ujian.Skor) AS 'Avg Skor', AVG(Ujian.Durasi) AS 'Avg Durasi'  FROM Ujian JOIN MasterSoal ON Ujian.IdMasterSoal = MasterSoal.IdMasterSoal JOIN Bahasa ON MasterSoal.IdBahasa = Bahasa.IdBahasa JOIN lvl ON  MasterSoal.IdLevel = lvl.IdLevel GROUP BY MasterSoal.IdMasterSoal";
             $res = $this->db->executeSelectQuery($statistik);
             return View::createView('hal_statistik-manajer.php',[
                 "nama" => $nama,
--- View/hal_statistik-manajer.php
+++ View/hal_statistik-manajer.php
@@ -7,11 +7,14 @@
                     <th>Bahasa</th>
                     <th>Level</th>
                     <th>Rata-Rata Skor</th>
+                    <th>Rata-Rata Durasi</th>
                 </tr>
                 <?php
                         $counter = 0;
                         for ($x = 0; $x < sizeof($statistik); $x++) {
                             echo"<tr>";
+                            echo "<td>".$statistik[$x][$counter]."</td>";
+                            $counter = $counter+1;
                             echo "<td>".$statistik[$x][$counter]."</td>";
                             $counter = $counter+1;
                             echo "<td>".$statistik[$x][$counter]."</td>";
