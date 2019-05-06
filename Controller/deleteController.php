c985812b74d589d5c5f21b867fd7a2d14e94a5b3
Henrico <henricoleodra@gmail.com>
2019-05-06 11:29:45 +0700

    Delete

--- Controller/deleteController.php
+++ Controller/deleteController.php
@@ -8,8 +8,21 @@ class deleteController{
         $this->db = new mysqlDB("localhost", "root", "", "FunCoding");
     }
     public function start(){
+        $cek = 'SELECT "Username" FROM "Ujian" WHERE "Username" = "'.$_GET['id'].'"';
+        if(sizeof($cek)==0){
         $query = 'DELETE FROM `pengguna` WHERE `Username` = "'.$_GET['id'].'"';
         $this->db->executeNonSelectQuery($query);
+
+        }
+        else{
+            $query = 'DELETE FROM `Ujian` WHERE `Username` = "'.$_GET['id'].'"';
+            $this->db->executeNonSelectQuery($query);
+            $query = 'DELETE FROM `penggunasoal` WHERE `Username` = "'.$_GET['id'].'"';
+            $this->db->executeNonSelectQuery($query);
+            $query = 'DELETE FROM `pengguna` WHERE `Username` = "'.$_GET['id'].'"';
+            $this->db->executeNonSelectQuery($query);
+
+        }
     }
 }
 
